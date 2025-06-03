28e4ace9b81f747392036fb7315da0a7967e104c
Srinivasan3454 <Srinivasansankaran25@gmail.com>
2025-06-03 18:01:06 +0530

    HAI HOW R U

---
+++ src/org/ClassNew.java
@@ -0,0 +1,18 @@
+package org;
+
+public class ClassNew {
+	private void add() {
+	System.out.println("task--1");
+
+	}
+  private void print() {
+	System.out.println("task--2");
+}
+  
+  public static void main(String[] args) {
+	ClassNew n = new ClassNew();
+	n.add();
+	n.print();
+}
+  
+}
